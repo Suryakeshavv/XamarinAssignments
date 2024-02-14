910c9e8d4797b66b07efd7c69b75fcd37d329c2c
K N <surya.kn@dxc.com>
2024-02-14 11:44:15 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Create one login screen with username and password.
+as of now hardcoded like admin/admin
+use stacklayout and entry control with button.
+ 
+place those into center.
+ 
+if login sucecss then use displayalert as login success
+if not then display alert with login failed... try again
+ 
---
+++ TrainingDay3Assignment/LoginPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TrainingDay3Assignment
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if(uName.Text == "admin" && pass.Text == "admin")
+            {
+                DisplayAlert("Login Success", "Success", "OK");
+                uName.Text = null; pass.Text = null;
+
+
+            }
+            else 
+            {
+                DisplayAlert("Login Failed", "Login Failed! Try again.", "OK");
+
+                uName.Text = null; pass.Text = null;
+
+
+            }
+        }
+    }
+}
